4d2a8d857a896bb5ded118e943a37c5988bd59c5
AsaZelva <130776232+AsaZelva@users.noreply.github.com>
2023-04-14 17:12:00 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+music.playMelody("A F E F D G E F ", 120)
+music.playMelody("B G - A F C5 D E ", 120)
+music.playMelody("C5 G D B E A C G ", 120)
+music.playMelody("F E D C C D E F ", 120)
+basic.forever(function () {
+    basic.showIcon(IconNames.Asleep)
+})
